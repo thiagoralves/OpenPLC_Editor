0afc00f0fe079aea284f20d2d0774719a45b906c
Zarek Vane <zarek.vane@outlook.com>
2022-05-04 17:11:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ $? -ne 0 ]; then
     sudo dpkg -i ./wxpython/python3-wxgtk4.0_4.0.7+dfsg-10_amd64.deb
 fi
 
-pip3 install lxml zeroconf pyserial gnosis simplejson pyjamas nevow
+pip3 install lxml matplotlib zeroconf pyserial gnosis simplejson pyjamas nevow
 
 WORKING_DIR=$(pwd)
 echo -e "#!/bin/bash\n\
