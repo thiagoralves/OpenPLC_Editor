981add3461b783020fe61c6875670db94483cd68
Thiago Alves <thiagoralves@gmail.com>
2022-05-10 13:32:50 -0400

    fix: when triggering any file menu event, it popup "check update"

--- editor/BeremizIDE.py
+++ editor/BeremizIDE.py
@@ -255,6 +255,7 @@ class Beremiz(IDEFrame):
         AppendMenu(parent, help='', id=wx.ID_PRINT,
                    kind=wx.ITEM_NORMAL, text=_(u'Print') + '\tCTRL+P')
         parent.AppendSeparator()
+        id_update = wx.NewIdRef()
         AppendMenu(parent, help='', id=wx.ID_ANY,
                    kind=wx.ITEM_NORMAL, text=_(u'Check for updates...') + '\tCTRL+U')
         parent.AppendSeparator()
@@ -270,7 +271,7 @@ class Beremiz(IDEFrame):
         self.Bind(wx.EVT_MENU, self.OnPageSetupMenu, id=wx.ID_PAGE_SETUP)
         self.Bind(wx.EVT_MENU, self.OnPreviewMenu, id=wx.ID_PREVIEW)
         self.Bind(wx.EVT_MENU, self.OnPrintMenu, id=wx.ID_PRINT)
-        self.Bind(wx.EVT_MENU, self.OnUpdateMenu, id=wx.ID_ANY)
+        self.Bind(wx.EVT_MENU, self.OnUpdateMenu, id=wx.id_update)
         self.Bind(wx.EVT_MENU, self.OnQuitMenu, id=wx.ID_EXIT)
 
         self.AddToMenuToolBar([(wx.ID_NEW, "new", _(u'New'), None),
