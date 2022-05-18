4d4f529694922eec0c9e7066253188d3fef8db04
Zarek Vane <zarek.vane@outlook.com>
2022-05-18 14:37:18 +0800

    fix bug: param of wx.FileDialog is wrong

--- editor/ProjectController.py
+++ editor/ProjectController.py
@@ -2022,7 +2022,7 @@ class ProjectController(ConfigTreeNode, PLCControler):
             f = open(self._getIECgeneratedcodepath(), "r")	
             program = f.read()	
             f.close()	
-            dlg = wx.FileDialog(self.AppFrame, "Save to file:", "", "", "OpenPLC Program(*.st)|*.st", wx.SAVE|wx.OVERWRITE_PROMPT)	
+            dlg = wx.FileDialog(self.AppFrame, "Save to file:", "", "", "OpenPLC Program(*.st)|*.st", wx.FD_SAVE|wx.FD_OVERWRITE_PROMPT)	
             if dlg.ShowModal() == wx.ID_OK:	
                 try:	
                     f = open(dlg.GetPath(), "w")	
