7ee341b70aa0148e884814d42e9d088ad46f9c6f
Thiago Alves <thiagoralves@gmail.com>
2023-04-19 15:28:13 -0400

    Update version.py

--- editor/version.py
+++ editor/version.py
@@ -214,6 +214,6 @@ def GetAboutDialogInfo():
 
 app_version = "2.01"
 #rev = GetAppRevision()
-rev = "Release: 2023-04-13"
+rev = "Release: 2023-04-19"
 if rev is not None:
     app_version = app_version + "\n" + rev.rstrip()
