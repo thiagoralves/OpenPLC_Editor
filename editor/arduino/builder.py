2ae241a235dd11d36640ba0bc6a014552243cfee
Francesco Fioretti <56032486+Francesco-cpu@users.noreply.github.com>
2023-07-27 12:14:47 +0200

    Update builder.py with json for iruino

--- editor/arduino/builder.py
+++ editor/arduino/builder.py
@@ -238,6 +238,7 @@ https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectron
 https://raw.githubusercontent.com/CONTROLLINO-PLC/CONTROLLINO_Library/master/Boards/package_ControllinoHardware_index.json \
 https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json \
 https://facts-engineering.gitlab.io/facts-open-source/p1am/beta_file_hosting/package_productivity-P1AM_200-boardmanagermodule_index.json \
+https://raw.githubusercontent.com/VEA-SRL/IRUINO_Library/main/package_vea_index.json \
 "2>&1"')
         wx.CallAfter(txtCtrl.SetValue, compiler_logs)
         wx.CallAfter(scrollToEnd, txtCtrl)
@@ -250,7 +251,8 @@ https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32
 https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectronics_index.json \
 https://raw.githubusercontent.com/CONTROLLINO-PLC/CONTROLLINO_Library/master/Boards/package_ControllinoHardware_index.json \
 https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json \
-https://facts-engineering.gitlab.io/facts-open-source/p1am/beta_file_hosting/package_productivity-P1AM_200-boardmanagermodule_index.json')
+https://facts-engineering.gitlab.io/facts-open-source/p1am/beta_file_hosting/package_productivity-P1AM_200-boardmanagermodule_index.json \
+https://raw.githubusercontent.com/VEA-SRL/IRUINO_Library/main/package_vea_index.json')
         wx.CallAfter(txtCtrl.SetValue, compiler_logs)
         wx.CallAfter(scrollToEnd, txtCtrl)
 
