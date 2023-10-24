bf469e7f2d24835e911c1ea09373fa8161e76129
Thiago Alves <thiagoralves@gmail.com>
2023-10-24 14:43:09 -0400

    Added support for PWM_CONTROLLER to Arduino Uno

--- editor/arduino/src/hal/uno_leonardo_nano_micro_zero.cpp
+++ editor/arduino/src/hal/uno_leonardo_nano_micro_zero.cpp
@@ -5,6 +5,12 @@ extern "C" {
 #include "Arduino.h"
 #include "../examples/Baremetal/defines.h"
 
+#if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
+    #include "SAMD_PWM.h"
+#else
+    #include "AVR_PWM.h"
+#endif
+
 //OpenPLC HAL for Arduino Uno and Arduino Nano (old) form factor (Uno, Leonardo, Nano, Micro and Zero)
 
 /******************PINOUT CONFIGURATION*******************
@@ -19,6 +25,19 @@ uint8_t pinMask_DIN[] = {PINMASK_DIN};
 uint8_t pinMask_AIN[] = {PINMASK_AIN};
 uint8_t pinMask_DOUT[] = {PINMASK_DOUT};
 uint8_t pinMask_AOUT[] = {PINMASK_AOUT};
+
+#define NUM_OF_PWM_PINS       2
+#define PWM_DEFAULT_FREQ      490
+#define PWM_CHANNEL_0_PIN     9
+#define PWM_CHANNEL_1_PIN     10
+
+#if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
+    SAMD_PWM *PWM_Instance[NUM_OF_PWM_PINS];
+#else
+    AVR_PWM *PWM_Instance[NUM_OF_PWM_PINS];
+#endif
+
+extern "C" uint8_t set_hardware_pwm(uint8_t, float, float); //this call is required for the C-based PWM block on the Editor
 
 void hardwareInit()
 {
@@ -45,6 +64,41 @@ void hardwareInit()
 		uint8_t pin = pinMask_AOUT[i];
         pinMode(pin, OUTPUT);
     }
+
+    // Initialize PWM pins
+    #if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
+        PWM_Instance[0] = new SAMD_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
+        PWM_Instance[1] = new SAMD_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+    #else
+        PWM_Instance[0] = new AVR_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
+        PWM_Instance[1] = new AVR_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+    #endif
+}
+
+uint8_t set_hardware_pwm(uint8_t ch, float freq, float duty)
+{
+    const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN};
+
+    // If this call is being used, disable pins from regular analogWrite
+    for (int i = 0; i < NUM_ANALOG_OUTPUT; i++)
+    {
+        if (pinMask_AOUT[i] == PWM_CHANNEL_0_PIN || pinMask_AOUT[i] == PWM_CHANNEL_1_PIN)
+        {
+            pinMask_AOUT[i] = 255;
+        }
+    }
+
+    if (ch > 1)
+    {
+        return 0;
+    }
+
+    if (PWM_Instance[ch]->setPWM(pins[ch], freq, duty))
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 void updateInputBuffers()
