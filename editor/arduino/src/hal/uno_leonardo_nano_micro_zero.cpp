73501e58ec2fdeedce5551318fc856717ee0ef75
Thiago Alves <thiagoralves@gmail.com>
2023-10-26 16:00:53 -0400

    Added pin11 PWM for Uno boards

--- editor/arduino/src/hal/uno_leonardo_nano_micro_zero.cpp
+++ editor/arduino/src/hal/uno_leonardo_nano_micro_zero.cpp
@@ -26,10 +26,17 @@ uint8_t pinMask_AIN[] = {PINMASK_AIN};
 uint8_t pinMask_DOUT[] = {PINMASK_DOUT};
 uint8_t pinMask_AOUT[] = {PINMASK_AOUT};
 
-#define NUM_OF_PWM_PINS       2
 #define PWM_DEFAULT_FREQ      490
 #define PWM_CHANNEL_0_PIN     9
 #define PWM_CHANNEL_1_PIN     10
+
+#ifdef __AVR_ATmega32U4__
+    #define NUM_OF_PWM_PINS       2
+#else
+    #define NUM_OF_PWM_PINS       3
+    #define PWM_CHANNEL_2_PIN     11
+#endif
+
 
 #if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
     SAMD_PWM *PWM_Instance[NUM_OF_PWM_PINS];
@@ -71,11 +78,20 @@ void hardwareInit()
 void init_pwm()
 {
     // If PWM_CONTROLLER block is being used, disable pins from regular analogWrite
+    #ifdef __AVR_ATmega32U4__
+        const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN};
+    #else
+	const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN, PWM_CHANNEL_2_PIN};
+    #endif
+    
     for (int i = 0; i < NUM_ANALOG_OUTPUT; i++)
     {
-        if (pinMask_AOUT[i] == PWM_CHANNEL_0_PIN || pinMask_AOUT[i] == PWM_CHANNEL_1_PIN)
+        for (int j = 0; j < NUM_OF_PWM_PINS; j++)
         {
-            pinMask_AOUT[i] = 255;
+            if (pinMask_AOUT[i] == pins[j])
+            {
+                pinMask_AOUT[i] = 255; //disable pin
+            }
         }
     }
 
@@ -83,9 +99,16 @@ void init_pwm()
     #if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
         PWM_Instance[0] = new SAMD_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
         PWM_Instance[1] = new SAMD_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+	PWM_Instance[2] = new SAMD_PWM(PWM_CHANNEL_2_PIN, PWM_DEFAULT_FREQ, 0);
     #else
-        PWM_Instance[0] = new AVR_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
-        PWM_Instance[1] = new AVR_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+	#ifdef __AVR_ATmega32U4__
+            PWM_Instance[0] = new AVR_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
+            PWM_Instance[1] = new AVR_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+	#else
+	    PWM_Instance[0] = new AVR_PWM(PWM_CHANNEL_0_PIN, PWM_DEFAULT_FREQ, 0);
+            PWM_Instance[1] = new AVR_PWM(PWM_CHANNEL_1_PIN, PWM_DEFAULT_FREQ, 0);
+	    PWM_Instance[2] = new AVR_PWM(PWM_CHANNEL_2_PIN, PWM_DEFAULT_FREQ, 0);
+	#endif
     #endif
 }
 
@@ -97,9 +120,13 @@ uint8_t set_hardware_pwm(uint8_t ch, float freq, float duty)
         pwm_initialized = true;
     }
 
-    const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN};
+    #ifdef __AVR_ATmega32U4__
+        const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN};
+    #else
+	const uint8_t pins[] = {PWM_CHANNEL_0_PIN, PWM_CHANNEL_1_PIN, PWM_CHANNEL_2_PIN};
+    #endif
 
-    if (ch > 1)
+    if (ch >= NUM_OF_PWM_PINS)
     {
         return 0;
     }
@@ -116,14 +143,14 @@ void updateInputBuffers()
 {
     for (int i = 0; i < NUM_DISCRETE_INPUT; i++)
     {
-		uint8_t pin = pinMask_DIN[i];
+        uint8_t pin = pinMask_DIN[i];
         if (bool_input[i/8][i%8] != NULL) 
             *bool_input[i/8][i%8] = digitalRead(pin);
     }
     
     for (int i = 0; i < NUM_ANALOG_INPUT; i++)
     {
-		uint8_t pin = pinMask_AIN[i];
+        uint8_t pin = pinMask_AIN[i];
         if (int_input[i] != NULL)
             *int_input[i] = (analogRead(pin) * 64);
     }
@@ -133,13 +160,13 @@ void updateOutputBuffers()
 {
     for (int i = 0; i < NUM_DISCRETE_OUTPUT; i++)
     {
-		uint8_t pin = pinMask_DOUT[i];
+        uint8_t pin = pinMask_DOUT[i];
         if (bool_output[i/8][i%8] != NULL) 
             digitalWrite(pin, *bool_output[i/8][i%8]);
     }
     for (int i = 0; i < NUM_ANALOG_OUTPUT; i++)
     {
-		uint8_t pin = pinMask_AOUT[i];
+        uint8_t pin = pinMask_AOUT[i];
         if (int_output[i] != NULL) 
             analogWrite(pin, (*int_output[i] / 256));
     }
