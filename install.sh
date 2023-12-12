02f3a641c3b9b497589a42120b728a8b275f89e3
Thiago Alves <thiagoralves@gmail.com>
2023-12-12 15:05:29 -0500

    Upgrade pip before using it

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ fi
 
 #Installing Python dependencies
 python3.8 -m venv "$VENV_DIR"
+"$VENV_DIR/bin/python" -m pip install --upgrade pip
 "$VENV_DIR/bin/python" -m pip install wheel jinja2 lxml==4.6.2 future matplotlib zeroconf pyserial pypubsub pyro5 attrdict3
 "$VENV_DIR/bin/python" -m pip install wxPython==4.2.1 
 
