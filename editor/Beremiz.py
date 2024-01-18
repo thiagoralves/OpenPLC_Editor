20a0925e1c0878044dcdeb3f5f58426bcd35563b
Autonomy Server <autonomyserver@Autonomys-Mac-mini.local>
2024-01-18 16:00:09 -0500

    Windows path fix

--- editor/Beremiz.py
+++ editor/Beremiz.py
@@ -27,6 +27,8 @@ import os
 import sys
 import getopt
 
+sys.path.insert(0,'.\editor') #Fix for Windows
+
 import wx
 from wx.lib.agw.advancedsplash import AdvancedSplash, AS_NOTIMEOUT, AS_CENTER_ON_SCREEN
 
