9d0818237bc7cdca7eb8e2d873230e314d99a5d5
Thiago Alves <thiagoralves@gmail.com>
2024-01-25 10:52:55 -0500

    Update python3.8 to python3.9

--- install.sh
+++ install.sh
@@ -12,25 +12,25 @@ echo "[INSTALLING DEPENDENCIES]"
 if [ -x /bin/yum ]; then
     yum clean expire-cache
     yum check-update
-    sudo yum -q -y install make automake gcc gcc-c++ bison flex autoconf git python3.8 python3-devel libxml2-devel libxslt-devel gtk3-devel
+    sudo yum -q -y install make automake gcc gcc-c++ bison flex autoconf git python3.9 python3-devel libxml2-devel libxslt-devel gtk3-devel
 #Installing dependencies for Ubuntu/Mint/Debian
 elif [ -x /usr/bin/apt-get ]; then
     sudo apt-get -qq update
     sudo apt-get install -y build-essential bison flex autoconf \
                           automake make git libgtk-3-dev\
-                          python3.8 python3-venv
+                          python3.9 python3-venv
 #Installing dependencies for opensuse tumbleweed
 elif [ -x /usr/bin/zypper ]; then
     sudo zypper ref
     sudo zypper in -y make automake gcc gcc-c++ bison flex autoconf
-    sudo zypper in -y python python-xml python3.8 python3-pip
+    sudo zypper in -y python python-xml python3.9 python3-pip
 else
     echo "Unsupported linux distro."
     exit 1
 fi
 
 #Installing Python dependencies
-python3.8 -m venv "$VENV_DIR"
+python3.9 -m venv "$VENV_DIR"
 "$VENV_DIR/bin/python" -m pip install --upgrade pip
 "$VENV_DIR/bin/python" -m pip install wheel jinja2 lxml==4.6.2 future matplotlib zeroconf pyserial pypubsub pyro5 attrdict3
 "$VENV_DIR/bin/python" -m pip install wxPython==4.2.0 
